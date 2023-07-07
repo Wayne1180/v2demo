195bf86380cbb707df74f632182097050c4b360e
Wayne1180 <xutenglongxutenglong@outlook.com>
2023-07-07 12:41:58 +0800

    ✨ feat: component: drag

--- src/main.js
+++ src/main.js
@@ -1,9 +1,27 @@
+/*
+ * @Author: 徐腾龙
+ * @Date: 2023-07-07 10:28:52
+ * @LastEditors: 徐腾龙
+ * @LastEditTime: 2023-07-07 10:45:00
+ * @Description: 
+ * @FilePath: \v2demo\src\main.js
+ */
 import Vue from 'vue'
+import Antd from 'ant-design-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'ant-design-vue/dist/antd.css'
+import 'normalize.css/normalize.css'
+
+// 引入组件
+import Drag from '@/components/Drag/drag.vue'
 
 Vue.config.productionTip = false
+
+Vue.use(Antd)
+
+Vue.component('Drag', Drag)
 
 new Vue({
   router,
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,55 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-home">
+    <Drag>
+      <div class="content">
+        <h1 class="noselect">Drag Me!</h1>
+        <!-- <a-button type="primary">ClickMe</a-button> -->
+      </div>
+    </Drag>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  methods: {
+    handleTouchStart(event) {
+      console.log("touchstart");
+    },
+    handleTouchEnd(e) {
+      console.log(e);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.container-home {
+  position: relative;
+  width: 100vw;
+  height: 100%;
+  background-image: linear-gradient(0deg, #6a85b6 0%, #bac8e0 100%);
+  .content {
+    text-align: center;
+    line-height: 300px;
+    width: 300px;
+    height: 300px;
+    border-radius: 50%;
+    background-image: linear-gradient(
+      0deg,
+      #fad0c4 0%,
+      #fad0c4 1%,
+      #ffd1ff 100%
+    );
+    .noselect,
+    [noselect] {
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+
+      -webkit-touch-callout: none;
+      -khtml-user-select: none;
+    }
   }
 }
-</script>
+</style>
